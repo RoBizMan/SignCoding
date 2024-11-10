73ab01fd9387704b18821c29620d4cbd6e9605e9
Robert Tiffert <robert@tiffert.eu>
2024-11-10 14:34:48 +0000

    Create models for tutor app and load data via json files

--- tutor/admin.py
+++ tutor/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Tutor, ProgrammingLanguage, SignLanguage, DayAvailability, TimeSlot
 
-# Register your models here.
+@admin.register(Tutor)
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ('tutor_firstname', 'tutor_lastname', 'tutor_email', 'price')  # Display these fields in the list view
+    search_fields = ('tutor_firstname', 'tutor_lastname', 'tutor_email')  # Add search functionality
+    list_filter = ('programming_languages', 'sign_languages', 'day_availability')  # Add filters for better navigation
+
+@admin.register(ProgrammingLanguage)
+class ProgrammingLanguageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(SignLanguage)
+class SignLanguageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(DayAvailability)
+class DayAvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+    def get_queryset(self, request):
+        # Limit queryset to only the predefined days
+        return super().get_queryset(request).filter(name__in=[
+            'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+        ])
+
+    def has_add_permission(self, request):
+        # Prevent adding new entries
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        # Prevent deletion of existing entries
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        # Allow changing existing entries if needed
+        return False
+
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('formatted_start_time', 'formatted_end_time')
+    search_fields = ('start_time', 'end_time')
+
+    def formatted_start_time(self, obj):
+        return obj.start_time.strftime('%H:%M')
+    formatted_start_time.short_description = 'Start Time'
+
+    def formatted_end_time(self, obj):
+        return obj.end_time.strftime('%H:%M')
+    formatted_end_time.short_description = 'End Time'
--- tutor/models.py
+++ tutor/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class ProgrammingLanguage(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class SignLanguage(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class DayAvailability(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    order = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = "Day Availability"
+        verbose_name_plural = "Day Availabilities"
+
+
+class TimeSlot(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.start_time.strftime('%H:%M')} - {self.end_time.strftime('%H:%M')}"
+
+
+class Tutor(models.Model):
+    tutor_firstname = models.CharField(max_length=50, verbose_name="Tutor's First Name")
+    tutor_lastname = models.CharField(max_length=50, verbose_name="Tutor's Surname")
+    tutor_email = models.EmailField(max_length=254, unique=True, verbose_name="Tutor's Email Address")
+    programming_languages = models.ManyToManyField(ProgrammingLanguage)
+    sign_languages = models.ManyToManyField(SignLanguage)
+    day_availability = models.ManyToManyField(DayAvailability)
+    time_availability = models.ManyToManyField(TimeSlot)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    photo = models.ImageField(upload_to='tutor_images/', default='tutor_images/default.jpg')
+    
+    def __str__(self):
+        return f"{self.tutor_firstname} {self.tutor_lastname} | {self.tutor_email}"
