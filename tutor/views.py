c98532d8c3d0f3eee996d44a9bc214da2728d687
Robert Tiffert <robert@tiffert.eu>
2024-11-11 19:46:31 +0000

    Limit list of tutors to 5 per page and add pagination control

--- tutor/views.py
+++ tutor/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from .models import Tutor
+from django.core.paginator import Paginator
 
-# Create your views here.
 def tutors(request):
-    tutors = Tutor.objects.all()
+    tutors_list = Tutor.objects.all()
 
     # Limit pills of sign languages to two plus more in a pill in the frontend view
-    for tutor in tutors:
+    for tutor in tutors_list:
         programming_languages = tutor.programming_languages.all()
         sign_languages = tutor.sign_languages.all()
         tutor.remaining_programming_languages = max(0, len(programming_languages) - 4)
         tutor.remaining_sign_languages = max(0, len(sign_languages) - 2)
 
-    return render(request, 'tutor/tutors.html', {'tutors': tutors})
+    # Pagination
+    paginator = Paginator(tutors_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'tutor/tutors.html', {'page_obj': page_obj})
