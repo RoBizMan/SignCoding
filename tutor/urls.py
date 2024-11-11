146f10de2a17370c49c9b2e2e661cfa0ee0bc3e9
Robert Tiffert <robert@tiffert.eu>
2024-11-11 08:44:43 +0000

    Update base.html to include all hrefs and duplicate base.html for Django templates

--- tutor/urls.py
+++ tutor/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path('tutors/', include('tutor.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.tutors, name='tutors'),
+]
--- tutor/views.py
+++ tutor/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'home/tutor.html')
+def tutors(request):
+    return render(request, 'tutor/tutors.html')
