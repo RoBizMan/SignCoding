17f694ca23cdb628b8bbfe19546750860fe08dd6
Robert Tiffert <robert@tiffert.eu>
2024-11-25 16:10:59 +0000

    Add email confirmation for newsletter subscription

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,13 +1,36 @@
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import NewsletterSubscription
 
+# Function to send the newsletter subscription confirmation email
+def send_newsletter_confirmation_email(email):
+    """
+    Sends a confirmation email to the user after a successful newsletter subscription.
+    """
+    # Render email subject and body from templates
+    subject = render_to_string('emails/newsletter_confirmation_email_subject.txt')
+    body = render_to_string('emails/newsletter_confirmation_email_body.html', {'email': email})
+
+    # Send email using Django's send_mail function
+    send_mail(
+        subject.strip(),  # Subject line
+        settings.DEFAULT_FROM_EMAIL,  # From email
+        [email],          # To email
+        html_message=body  # HTML content
+    )
+
+# Newsletter subscription view
 def subscribe_newsletter(request):
     if request.method == "POST":
         email = request.POST.get('email')
         if NewsletterSubscription.objects.filter(email=email).exists():
             messages.error(request, "You have already subscribed to our newsletter!")
         else:
-            NewsletterSubscription.objects.create(email=email)
-            messages.success(request, "Thank you for subscribing to our newsletter!")
+            subscription = NewsletterSubscription.objects.create(email=email)
+            # Send confirmation email
+            send_newsletter_confirmation_email(subscription.email)
+            messages.success(request, "Thank you for subscribing to our newsletter! Check your email for confirmation.")
     return redirect(request.META.get('HTTP_REFERER', '/'))  # Redirect back to the referring page
